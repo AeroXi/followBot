243b2675139d9bd3395684d3c133bdc6acbfa651
AeroXi <chenxi@mistgpu.com>
2022-04-30 21:32:48 +0800

    fix for mintTo

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 const whaleAddress = require("./watch_address")
-const logWhaleAddress = whaleAddress.map(address => `0x000000000000000000000000${address.slice(2)}`)
+const logWhaleAddress = whaleAddress.map(
+  (address) => `0x000000000000000000000000${address.slice(2)}`
+)
 
 require("dotenv").config()
 const axios = require("axios")
 const API_URL = process.env.MAINNET
 const PUBLIC_KEY = process.env.PUBLIC_KEY
 const PRIVATE_KEY = process.env.PRIVATE_KEY
-const BARK_URL =  process.env.BARK_URL
+const BARK_URL = process.env.BARK_URL
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3")
 const web3 = createAlchemyWeb3(API_URL)
 const BN = web3.utils.BN
 let contractHistory = []
-let maxPriceWei = web3.utils.toWei('0.01', 'ether'); 
+let maxPriceWei = web3.utils.toWei("0.01", "ether")
 maxPriceWei = web3.utils.toBN(maxPriceWei)
-
 
 async function bark(title, message) {
   await axios.get(`${BARK_URL}${title}/${message}`)
+}
+
+// 处理mintTo(address)这种包含地址的input
+function handleInput(input, whale) {
+  // case insensitive compare
+  input = input.toLowerCase()
+  whale = whale.toLowerCase()
+  if (input.includes(whale.slice(2))) {
+    // 替换成bot地址
+    return input.replace(whale.slice(2), PUBLIC_KEY.slice(2))
+  }
+  else {
+    return input
+  }
 }
 
 async function mintNFT(contractAddress, inputData, value) {
@@ -63,20 +78,23 @@ const filter = {
 
 const handleTX = async (tx) => {
   if (
-    web3.utils.toBN(tx.value).lte(maxPriceWei) &&  // value <= maxPrice
+    web3.utils.toBN(tx.value).lte(maxPriceWei) && // value <= maxPrice
     contractHistory.includes(tx.to) === false &&
-    whaleAddress.includes(tx.from) 
+    whaleAddress.includes(tx.from)
   ) {
     contractHistory.push(tx.to)
+    let botInput = handleInput(tx.input, tx.from)
     try {
-      await mintNFT(tx.to, tx.input, tx.value)
-    }
-    catch (error) {
+      await mintNFT(tx.to, botInput, tx.value)
+    } catch (error) {
       console.log(error)
       await bark("bot error", error)
     }
     console.log(`contract: ${tx.to} input: ${tx.input} whale: ${tx.from}`)
-    await bark("bot minted", `contract: ${tx.to} input: ${tx.input} whale: ${tx.from}`)
+    await bark(
+      "bot minted",
+      `contract: ${tx.to} input: ${tx.input} whale: ${tx.from}`
+    )
   }
 }
 
@@ -87,3 +105,4 @@ const doSomethingWithTxn = (txn) => {
 
 // Open the websocket and listen for events!
 web3.eth.subscribe("logs", filter).on("data", doSomethingWithTxn)
+
